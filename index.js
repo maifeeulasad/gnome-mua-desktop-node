a4bceab2d1088a912b7e8c7533097e9becfa23bd
maifeeulasad <maifeeulasad@gmail.com>
2021-04-27 10:37:07 +0600

    request an image based on prompt

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const inquirer = require('inquirer');
+const axios = require('axios');
+
+let sourceUrl = "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json"
 
 clear();
 
@@ -31,10 +34,35 @@ const query = async () => {
     const prompts = await inquirer.prompt(questions);
     let validatedOrNot = prompts['validatedOrNot']
     if(validatedOrNot==='y' || validatedOrNot==='Y'){
-
+        fetchWallPaper(true);
     }else if(validatedOrNot==='n' || validatedOrNot==='N'){
-
+        fetchWallPaper(false);
     }
 };
   
 query();
+
+const fetchWallPaper = (validatedOrNot) => {
+    sourceUrl 
+        = validatedOrNot 
+        ? "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json" 
+        : "https://www.reddit.com/r/wallpaper/top.json"
+
+    axios
+        .get(sourceUrl)
+        .then((res)=>{
+            let backgroundImage = "https://i.imgur.com/lSdg2M9.jpg";
+            if(sourceUrl==="https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json"){
+                let responseJson = JSON.parse(JSON.stringify(res.data));
+                let randomIndex = Math.random() * responseJson.length | 0;
+                let randomImage = responseJson[randomIndex];
+                backgroundImage = randomImage.url;
+            }else{
+                let responseJson = JSON.parse(JSON.stringify(res.data)).data.children;
+			    let randomIndex = Math.random() * responseJson.length | 0;
+			    let randomImage = responseJson[randomIndex].data;
+			    backgroundImage = randomImage.url;
+            }
+            console.log(backgroundImage);
+        })
+}
