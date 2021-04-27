8ca90422ce08118aa27600d0823afa53322725ee
maifeeulasad <maifeeulasad@gmail.com>
2021-04-27 11:10:41 +0600

    beautify

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const http = require('http');
 const https = require('https');
 const shelljs = require('shelljs');
 
-var dir = os.homedir()+"/mua/gnome-mua-desktop";
+var dir = os.homedir() + "/mua/gnome-mua-desktop";
 
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir,{ recursive: true });
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, {
+        recursive: true
+    });
 }
 
 let sourceUrl = "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json"
@@ -22,88 +24,88 @@ const welcome = () => {
 
     console.log(
         chalk
-            .green(
-                figlet
-                    .textSync('Gnome MUA Desktop', { horizontalLayout: 'full' })
-            )
+        .green(
+            figlet
+            .textSync('Gnome MUA Desktop', {
+                horizontalLayout: 'full'
+            })
+        )
     )
 
     query();
 }
 
 const query = async () => {
-    const questions = [
-    {
-      name: 'validatedOrNot',
-      type: 'input',
-      message: 'Please enter [y/n] if you want all wallpaper from verified source or not :',
-      validate: function( value ) {
-        if (value==='y' || value==='Y' || value==='n' || value==='N') {
-          return true;
-        } 
-        return false;
-      }
-    },
-    ];
+    const questions = [{
+        name: 'validatedOrNot',
+        type: 'input',
+        message: 'Please enter [y/n] if you want all wallpaper from verified source or not :',
+        validate: function(value) {
+            if (value === 'y' || value === 'Y' || value === 'n' || value === 'N') {
+                return true;
+            }
+            return false;
+        }
+    }, ];
     const prompts = await inquirer.prompt(questions);
     let validatedOrNot = prompts['validatedOrNot']
-    if(validatedOrNot==='y' || validatedOrNot==='Y'){
+    if (validatedOrNot === 'y' || validatedOrNot === 'Y') {
         setInterval(function() {
             fetchWallPaper(true);
         }, 5000);
-    }else if(validatedOrNot==='n' || validatedOrNot==='N'){
+    } else if (validatedOrNot === 'n' || validatedOrNot === 'N') {
         setInterval(function() {
             fetchWallPaper(false);
         }, 5000);
     }
 };
-  
+
 
 const setWallpapper = (filePath) => {
-    const shellCommand = "gsettings set org.gnome.desktop.background picture-uri "+ filePath
+    const shellCommand = "gsettings set org.gnome.desktop.background picture-uri " + filePath
     shelljs.exec(shellCommand);
 }
 
 const downloadWallpaper = (url) => {
-    const filePath = dir
-                    +"/current" 
-                    + (sourceUrl === "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json"
-                        ? ".jpg"
-                        : ".png");
+    const filePath = dir +
+        "/current" +
+        (sourceUrl === "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json" ?
+            ".jpg" :
+            ".png");
     const file = fs.createWriteStream(filePath);
-    if(url.includes("https")){
+    if (url.includes("https")) {
         https.get(url, function(response) {
             response.pipe(file);
             setWallpapper(filePath);
-          });
-    }else{
+        });
+    } else {
         http.get(url, function(response) {
             response.pipe(file);
             setWallpapper(filePath);
-          });
+        });
     }
 }
 
 const fetchWallPaper = (validatedOrNot) => {
-    sourceUrl 
-        = validatedOrNot 
-        ? "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json" 
-        : "https://www.reddit.com/r/wallpaper/top.json"
+    sourceUrl
+        = validatedOrNot ?
+        "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json" :
+        "https://www.reddit.com/r/wallpaper/top.json"
 
     axios
         .get(sourceUrl)
-        .then((res)=>{
+        .then((res) => {
             let backgroundImage = "https://i.imgur.com/lSdg2M9.jpg";
-            if(sourceUrl==="https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json"){
+            if (sourceUrl === "https://raw.githubusercontent.com/maifeeulasad/chrome-mua-tab/data-source/data.json") {
                 let responseJson = JSON.parse(JSON.stringify(res.data));
                 let randomIndex = Math.random() * responseJson.length | 0;
                 let randomImage = responseJson[randomIndex];
                 backgroundImage = randomImage.url;
-            }else{
+            } else {
                 let responseJson = JSON.parse(JSON.stringify(res.data)).data.children;
-			    let randomIndex = Math.random() * responseJson.length | 0;
-			    let randomImage = responseJson[randomIndex].data;
-			    backgroundImage = randomImage.url;
+                let randomIndex = Math.random() * responseJson.length | 0;
+                let randomImage = responseJson[randomIndex].data;
+                backgroundImage = randomImage.url;
             }
             downloadWallpaper(backgroundImage);
         })
